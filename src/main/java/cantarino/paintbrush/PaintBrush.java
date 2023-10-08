0203969683794a7f49289f897b7f2dcb6bc8be47
CantarinoG <guilhermecantarino1709@gmail.com>
2023-10-08 16:27:43 -0300

    Add JFrame

--- src/main/java/cantarino/paintbrush/PaintBrush.java
+++ src/main/java/cantarino/paintbrush/PaintBrush.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package cantarino.paintbrush;
 
-/**
- *
- * @author Cantarino
- */
 public class PaintBrush {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        frmPaint frm = new frmPaint();
+        frm.setVisible(true);
     }
 }
