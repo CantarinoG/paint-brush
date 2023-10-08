c4be95f28759b217c9cbcb5f0b7d2908a8417508
CantarinoG <guilhermecantarino1709@gmail.com>
2023-10-08 11:11:15 -0300

    Add initial project

---
+++ src/main/java/cantarino/paintbrush/PaintBrush.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cantarino.paintbrush;
+
+/**
+ *
+ * @author Cantarino
+ */
+public class PaintBrush {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
